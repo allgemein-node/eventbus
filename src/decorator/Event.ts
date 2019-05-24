d65236698018f0646d490aeed1c5b84fa1f7976a
Robert Kania <cezaryrk@yahoo.de>
2019-05-24 12:40:38 +0200

    fix: remove return from annotations, causes reflect error

--- src/decorator/Event.ts
+++ src/decorator/Event.ts
@@ -2,6 +2,6 @@ import EventBusMeta from '../bus/EventBusMeta';
 
 export function Event(): Function {
   return function (object: Function) {
-    return EventBusMeta.$().registerEventClass(object);
+    EventBusMeta.$().registerEventClass(object);
   };
 }
